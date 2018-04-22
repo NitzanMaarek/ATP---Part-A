76ba2835460458f342a840269d39e6061d71e724
chenyanai <chenyanai@gmail.com>
2018-04-22 18:15:39 +0300

    second commit

--- IdeaProjects/ATP – Project - PartA/src/algorithms/mazeGenerators/AMazeGenerator.java
+++ IdeaProjects/ATP – Project - PartA/src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -1,4 +1,16 @@
 package algorithms.mazeGenerators;
 
-public class AMazeGenerator {
+abstract public class AMazeGenerator
+    implements IMazeGenerator {
+
+    @Override
+    abstract public Maze generate(int x, int y);
+
+    @Override
+    public long measureAlgorithmTimeMillis(int x, int y) {
+        long a = System.currentTimeMillis();
+        generate(x, y);
+        long b = System.currentTimeMillis();
+        return b - a;
+    }
 }
--- IdeaProjects/ATP – Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
+++ IdeaProjects/ATP – Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -1,4 +1,7 @@
 package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
+
+    public Maze generate(int x, int y);
+    public long measureAlgorithmTimeMillis(int x, int y);
 }
